5ba3b03a908c6ebe8f322cfeee3ab8445146c95c
Mike Maelzer <mmaelzer@3vr.com>
2012-12-24 10:40:09 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Shiminy
+=======
+
+A small .NET shim application that hides console applications. Shiminy expects a command prompt filename as the first argument followed by an optional set of additional arguments to be used when calling the command prompt application.
+
+---------------
+
+###Example Usage###
+	
+	c:\shiminy>shiminy c:\nodejs\node.exe c:\app\app.js
---
+++ Shiminy/Shim.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Shiminy
+{
+    static class Shim
+    {
+        /// <summary>
+        /// A small shim application that expects a command prompt filename as the first argument
+        /// proceeded by an optional set of additional arguments to be used when calling the
+        /// command prompt application
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            var process = new Process
+                {
+                    StartInfo =
+                        {
+                            CreateNoWindow = true,
+                            UseShellExecute = false, 
+                            FileName = args[0]
+                        }
+                };
+
+
+            if (args.Length > 1)
+            {
+                args[0] = string.Empty;
+                process.StartInfo.Arguments = string.Join(" ", args);
+            }
+
+            process.Start();
+        }
+    }
+}
